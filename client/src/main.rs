0ac9b178ac57395cd91f33ece778d284d1490e97
hash1018 <46293206+hash1018@users.noreply.github.com>
2023-06-01 08:35:53 +0900

    Add Login page file

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,8 @@
 use yew::html;
-use yew::html::Scope;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+use crate::pages::login::Login;
 use crate::pages::workspace::Workspace;
 
 mod pages;
@@ -23,50 +23,6 @@ fn switch(routes: Route) -> Html {
         Route::Workspace => {
             html! { <Workspace /> }
         }
-    }
-}
-
-pub enum Message {
-    LoginButtonClicked,
-}
-
-pub struct Login {}
-
-impl Component for Login {
-    type Message = Message;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Message::LoginButtonClicked => {
-                let navigator = ctx.link().navigator().unwrap();
-                navigator.push(&Route::Workspace);
-            }
-        }
-        true
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html!({ self.view_navi(ctx.link()) })
-    }
-}
-
-impl Login {
-    fn view_navi(&self, link: &Scope<Self>) -> Html {
-        let login_button_clicked = link.callback(|_| Message::LoginButtonClicked);
-
-        html!(
-            <body>
-                <div class="center">
-                    <input id="username" style="display:block; width:100px; box-sizing: border-box" type="text" placeholder="username" />
-                    <button onclick={login_button_clicked}> {"Login"} </button>
-                </div>
-            </body>
-        )
     }
 }
 
