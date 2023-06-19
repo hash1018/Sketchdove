ea8502a90fc4e3cce6e64a2431389589e3b4b832
hash1018 <46293206+hash1018@users.noreply.github.com>
2023-06-20 00:53:10 +0900

    Prevent zoom, contextmenu

--- frontend/src/pages/app.rs
+++ frontend/src/pages/app.rs
@@ -1,5 +1,7 @@
 use std::sync::Mutex;
-use yew::{function_component, html, Html};
+use wasm_bindgen::{prelude::Closure, JsCast};
+use web_sys::AddEventListenerOptions;
+use yew::{html, Component, Html};
 use yew_router::{BrowserRouter, Routable, Switch};
 
 use crate::pages::{main::Main, workspace::Workspace};
@@ -35,11 +37,99 @@ pub fn user_name() -> Option<String> {
     user_name
 }
 
-#[function_component]
-pub fn App() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} />
-        </BrowserRouter>
+pub struct App {
+    wheel_closure: Option<Closure<dyn FnMut(web_sys::WheelEvent)>>,
+    contextmenu_closure: Option<Closure<dyn FnMut(web_sys::MouseEvent)>>,
+}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &yew::Context<Self>) -> Self {
+        let wheel_closure = prevent_wheel_closure();
+        let contextmenu_closure = prevent_contextmenu_closure();
+
+        Self {
+            wheel_closure,
+            contextmenu_closure,
+        }
+    }
+
+    fn destroy(&mut self, _ctx: &yew::Context<Self>) {
+        if let Some(window) = web_sys::window() {
+            if let Some(closure) = self.wheel_closure.take() {
+                window
+                    .remove_event_listener_with_callback("wheel", closure.as_ref().unchecked_ref())
+                    .unwrap();
+            }
+
+            if let Some(closure) = self.contextmenu_closure.take() {
+                window
+                    .remove_event_listener_with_callback(
+                        "contextmenu",
+                        closure.as_ref().unchecked_ref(),
+                    )
+                    .unwrap();
+            }
+        }
+    }
+
+    fn update(&mut self, _ctx: &yew::Context<Self>, _msg: Self::Message) -> bool {
+        true
+    }
+
+    fn view(&self, _ctx: &yew::Context<Self>) -> Html {
+        html! {
+            <BrowserRouter>
+                <Switch<Route> render={switch} />
+            </BrowserRouter>
+        }
+    }
+}
+
+fn prevent_wheel_closure() -> Option<Closure<dyn FnMut(web_sys::WheelEvent)>> {
+    if let Some(window) = web_sys::window() {
+        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::WheelEvent| {
+            event.prevent_default();
+        });
+
+        let mut option = AddEventListenerOptions::new();
+        option.passive(false);
+
+        window
+            .add_event_listener_with_callback_and_add_event_listener_options(
+                "wheel",
+                closure.as_ref().unchecked_ref(),
+                &option,
+            )
+            .unwrap();
+
+        Some(closure)
+    } else {
+        None
+    }
+}
+
+fn prevent_contextmenu_closure() -> Option<Closure<dyn FnMut(web_sys::MouseEvent)>> {
+    if let Some(window) = web_sys::window() {
+        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
+            event.prevent_default();
+        });
+
+        let mut option = AddEventListenerOptions::new();
+        option.passive(false);
+
+        window
+            .add_event_listener_with_callback_and_add_event_listener_options(
+                "contextmenu",
+                closure.as_ref().unchecked_ref(),
+                &option,
+            )
+            .unwrap();
+
+        Some(closure)
+    } else {
+        None
     }
 }
